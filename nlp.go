5d3d64943f3072dc6324be13ba2d20eb03d506ef
Georgi Kamacharov <gkamacharov@gmail.com>
2019-06-09 22:32:33 -0400

    change to sorted intersect for o(n * log(n)) speed

--- nlp.go
+++ nlp.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"reflect"
+	"sort"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -35,9 +37,37 @@ func categorize(c *gin.Context) {
 
 func tokenize(content string) []string {
 	contentSplit := strings.FieldsFunc(content, split)
+	stopWords := []string{"ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during",
+		"out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such",
+		"into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each",
+		"the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me",
+		"were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up",
+		"to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have",
+		"in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did",
+		"not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those",
+		"i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how",
+		"further", "was", "here", "than"}
 
-	return contentSplit
+	return intersectSorted(contentSplit, stopWords).([]string)
 }
 func split(r rune) bool {
 	return r == ' ' || r == ',' || r == ';' || r == '!' || r == '?' || r == '.'
+}
+
+func intersectSorted(a interface{}, b interface{}) interface{} {
+	set := make([]interface{}, 0)
+	av := reflect.ValueOf(a)
+	bv := reflect.ValueOf(b)
+
+	for i := 0; i < av.Len(); i++ {
+		el := av.Index(i).Interface()
+		idx := sort.Search(bv.Len(), func(i int) bool {
+			return bv.Index(i).Interface() == el
+		})
+		if idx < bv.Len() && bv.Index(idx).Interface() == el {
+			set = append(set, el)
+		}
+	}
+
+	return set
 }
